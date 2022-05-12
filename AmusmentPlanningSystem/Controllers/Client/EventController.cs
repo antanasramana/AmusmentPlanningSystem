62f710e74bf2df3860242d47733b0fdbe3f6a0f7
mizge <ugne.glinskyte@ktu.edu>
2022-05-12 20:02:27 +0300

    ordered events done

---
+++ AmusmentPlanningSystem/Controllers/Client/EventController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AmusmentPlanningSystem.Data;
+using AmusmentPlanningSystem.Models;
+
+namespace AmusmentPlanningSystem.Controllers.Client
+{
+    public class EventController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EventController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+        }
+        public IActionResult OpenOrderedEventsList()
+        {
+            //here we will take client from session
+            var client = new Models.Client { UserId = 1, IsBlocked = false, Address = "K. Baršausko g. 67" };
+            var orders = _context.Orders.Where(order => order.ClientId == client.UserId).ToList();
+            var events = _context.Events.Include(eve => eve.Service).ToList().Join(orders, e => e.OrderId, order => order.Id, (e, order) => e).ToList();
+
+            return View("./Views/OrderedEvents/OrderedEventsList.cshtml", events);
+        }
+    }
+}
