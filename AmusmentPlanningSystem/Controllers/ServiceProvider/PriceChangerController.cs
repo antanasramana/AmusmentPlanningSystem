d9916cb786ad84d44a733e062dc65dea82d26aa9
Petras Vičiūnas <petrasviciunas@gmail.com>
2022-05-18 22:25:09 +0300

    Did stuff

--- AmusmentPlanningSystem/Controllers/ServiceProvider/PriceChangerController.cs
+++ AmusmentPlanningSystem/Controllers/ServiceProvider/PriceChangerController.cs
@@ -2,67 +2,92 @@
 using AmusmentPlanningSystem.Controllers.Client;
 using AmusmentPlanningSystem.Data;
 using AmusmentPlanningSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AmusmentPlanningSystem.Controllers.ServiceProvider
 {
-    public class PriceChangerController
+    public class PriceChangerController : Controller
     {
-        private readonly EventController _eventController;
-        private readonly ServiceController _serviceController;
         private readonly ApplicationDbContext _context;
 
-        public PriceChangerController(ApplicationDbContext context, EventController controller, ServiceController serviceController)
+        public PriceChangerController(ApplicationDbContext context)
         {
-            _eventController = controller;
             _context = context;
-            _serviceController = serviceController;
         }
 
-        public void CalculateNewPrices()
+        [HttpGet]
+        public IActionResult CalculateNewPrices()
         {
             var startDate = DateTime.Now.Subtract(TimeSpan.FromDays(30));
-            var eventsByTime = _eventController.GetAllEventsByTime(startDate, DateTime.Now);
+            var eventsByTime = _context
+                .Events
+                .Include(e => e.Order)
+                .Include(e => e.Service)
+                .Where(e => e.From >= startDate && e.To <= DateTime.Now)
+                .ToList();
 
-            var groupedEvents = GroupEventsBasedOnService(eventsByTime);
+            var groupedServiceEvents = GroupEventsBasedOnService(eventsByTime);
 
-            foreach(var keyVal in groupedEvents)
+            foreach(var keyVal in groupedServiceEvents)
             {
                 var service = keyVal.Value.First().Service;
                 var events = keyVal.Value;
                 
-                if (service.ApplyDiscount > 0)
+                if (service.ApplyDiscount == 0)
                 {
-                    var revenue = _serviceController.GetServiceRevenueOf30Days(service.Id);
+                    var revenue = GetServiceRevenueOf30Days(service.Id);
 
                     if (!IsCompanyMoreProfitable(events, revenue))
                     {
-                        // remove discount
-                        var dbService = _context
-                            .Services
-                            !.SingleOrDefault(s => s.Id == service.Id);
-
-                        // add 10% of discount
-                        dbService!.ApplyDiscount = service.Price * 0.1;
-                        _context.SaveChanges();
+                        AddDiscountByService(service.Id, service.Price * 0.1);
                     }
                 } 
                 else
                 {
-                    // remove discount
-                    var dbService = _context
-                        .Services
-                        !.SingleOrDefault(s => s.Id == service.Id);
-                    dbService!.ApplyDiscount = 0;
-                    _context.SaveChanges();
+                    RemoveDiscountByService(service.Id);
                 }
             }
+
+            return View("./Views/Home/ServiceProviderHomePage.cshtml");
         }
 
-       
+        private double GetServiceRevenueOf30Days(int serviceId)
+        {
+            var service = _context.Services
+                !.Include(s => s.Events)
+                .ThenInclude(e => e.Order)
+                .Single(s => s.Id == serviceId);
+
+            return service.Events
+                .Where(e => e.Order != null && e.Order.Payment != null && e.From.AddDays(30) >= DateTime.Now)
+                .Select(e => e!.Order.Sum)
+                .Sum();
+        }
+
+        private void AddDiscountByService(int serviceId, double discount)
+        {
+            var service = _context.Services
+              !.Include(s => s.Events)
+              .ThenInclude(e => e.Order)
+              .Single(s => s.Id == serviceId);
+            service.ApplyDiscount = discount;
+            _context.SaveChanges();
+        }
+
+        private void RemoveDiscountByService(int serviceId)
+        {
+            var service = _context.Services
+               !.Include(s => s.Events)
+               .ThenInclude(e => e.Order)
+               .Single(s => s.Id == serviceId);
+            service.ApplyDiscount = 0;
+            _context.SaveChanges();
+        }
         private bool IsCompanyMoreProfitable(List<Event> events, double revenue)
         {
             double revenueOf5Days = events
-                .Where(e => e.From.AddDays(5) >= DateTime.Now && e.From < DateTime.Now)
+                .Where(e => e.Order != null && e.Order.Payment != null && e.From.AddDays(5) >= DateTime.Now && e.From < DateTime.Now)
                 .Select(e => e.Order.Sum)
                 .Sum();
 
